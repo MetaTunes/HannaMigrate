7b87ac01c2d5fcab92739bab3116016c46540402
Mark Evens <MetaTunes@users.noreply.github.com>
2024-08-14 23:15:24 +0100

    v0.0.3
    Include optional parameters for overwrite and delete

--- HannaMigrate.module.php
+++ HannaMigrate.module.php
@@ -1,11 +1,10 @@
-<?php namespace ProcessWire;
-
-class HannaMigrate extends WireData implements Module {
+<?php
+class HannaMigrate extends \ProcessWire\WireData implements \ProcessWire\Module {
 	public static function getModuleInfo() {
 		return array(
 			'title' => 'Hanna Migrate',
 			'summary' => 'Migrate Hanna codes between sites',
-			'version' => '0.0.1',
+			'version' => '0.0.3',
 			'author' => 'Mark Evens',
 			'icon' => 'medium',
 			'requires' => ['ProcessWire>=3.0.200'],
@@ -31,14 +30,14 @@ class HannaMigrate extends WireData implements Module {
 			$exportAll[$h->name] = "!HannaCode:$h->name:" . base64_encode(json_encode($exportData)) . "/!HannaCode";
 		}
 		$directory = $this->migrationDirectory($migrationName);
-		if(!is_dir($directory)) if(!wireMkdir($directory, true, "0777")) {          // wireMkDir recursive
+		if(!is_dir($directory)) if(!\ProcessWire\wireMkdir($directory, true, "0777")) {          // wireMkDir recursive
 			throw new WireException("Unable to create migration directory: $directory");
 		}
 		$exportJson = json_encode($exportAll);
 		file_put_contents($directory . 'data.json', $exportJson);
 	}
 
-	public function importAll($migrationName = null) {
+	public function importAll($migrationName = null, $overwrite = false, $delete = false) {
 		$modules = $this->wire('modules');
 		$session = $this->wire('session');
 		$hm = $modules->get('ProcessHannaCode');
@@ -48,9 +47,20 @@ class HannaMigrate extends WireData implements Module {
 		$dataFile = (file_exists($directory . 'data.json'))
 			? file_get_contents($directory . 'data.json') : null;
 
-		$dataArray = json_decode($dataFile);
+		$dataArray = json_decode($dataFile, true);
 		if(!$dataArray) {
 			throw new WireException("Failed to json decode import file");
+		}
+
+		// Delete existing hanna codes which are not in the import, if delete is set to true
+		if($delete) {
+			$ha = $hm->hannaCodes()->getAll();
+			foreach($ha as $h) {
+				if(!array_key_exists($h->name, $dataArray)) {
+					$hm->hannaCodes()->delete($h);
+					$session->message($this->_("Deleted Hanna Code:") . " $h->name");
+				}
+			}
 		}
 
 		foreach($dataArray as $nameKey => $data) {
@@ -76,15 +86,20 @@ class HannaMigrate extends WireData implements Module {
 
 			$h = $hm->hannaCodes()->get($name);
 			if($h->id) {
-				$session->error($this->_("Hanna Code with  name $nameKey already exists"));
+				if($overwrite) {
+					$hm->hannaCodes()->delete($h);
+					$session->message($this->_("Replaced Hanna Code with  name $nameKey"));
+				} else {
+					$session->error($this->_("Hanna Code with  name $nameKey already exists"));
+				}
 			}
 
 			$data['type'] = (int)$data['type'];
-			if($data['type'] & HannaCode::typePHP && !$hm->hasPermission('hanna-code-php')) {
+			if($data['type'] & \ProcessWire\HannaCode::typePHP && !$hm->hasPermission('hanna-code-php')) {
 				throw new WireException("You don't have permission to add/edit PHP Hanna Codes");
 			}
 
-			$h = new HannaCode();
+			$h = new \ProcessWire\HannaCode();
 			$hm->wire($h);
 			$h->name = $name;
 			$h->type = $data['type'];
@@ -105,8 +120,8 @@ class HannaMigrate extends WireData implements Module {
 		$modules = $this->wire('modules');
 		$dbm = $modules->get('ProcessDbMigrate');
 		if($dbm) {
-			$migrationPage = ($migrationName) ? $dbm->migrations->get("template=$dbm->migrationTemplate, name=$migrationName") : new nullPage();
-			/* @var $migrationPage DbMigrationPage */
+			$migrationPage = ($migrationName) ? $dbm->migrations->get("template=$dbm->migrationTemplate, name=$migrationName") : new \ProcessWire\nullPage();
+			/* @var $migrationPage \ProcessWire\DbMigrationPage */
 			if($migrationPage->id > 0) {
 				$directory = $dbm->migrationsPath;
 				$migrationPath = $directory . $migrationPage->name . '/hanna-codes/';
